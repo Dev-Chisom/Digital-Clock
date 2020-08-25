52254fd95a60ecb7368d1cd0f7b8adadbb9cb14f
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-08-25 13:08:39 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const clock = document.querySelector('.clock');
+
+const tick = () =>{
+    const now = new Date ();
+
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    const html= `
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+    `
+
+    clock.innerHTML = html;
+
+};  setInterval(tick, 1000)
